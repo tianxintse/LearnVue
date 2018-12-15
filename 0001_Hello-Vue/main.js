dab675a15df16deb0701b375a4ce04470f375b54
tianxintse <xietx1995@gmail.com>
2018-12-15 17:29:12 +0800

    Making progress

---
+++ 0001_Hello-Vue/main.js
@@ -0,0 +1,6 @@
+app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+});
---
+++ 0002_V-bind/main.js
@@ -0,0 +1,6 @@
+app = new Vue({
+  el: '#app',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+});
